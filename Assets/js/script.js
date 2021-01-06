7ddc747fa7f04c80df590013a04ebc45f5f108d2
Nickpodski <Nickpodski@gmail.com>
2021-01-05 19:42:57 -0500

    added all password criteria conditions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,9 +6,7 @@ var pwCriteria = document.querySelector("#passwordCriteria")
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  // if (pwCriteria.getAttribute("class") === "visually-hidden") {
-  //   pwCriteria.setAttribute("class", "")
-  // }
+
   passwordText.value = password;
 
 }
@@ -25,17 +23,158 @@ function generatePassword() {
       var pwUpperCase = confirm("Do you want Upper Case letters?");
       var pwNumbers = confirm("Do you want numbers?");
       var pwSpecial = confirm("Do you want Special Characters?")
-      if (pwLowerCase|| pwUpperCase || pwNumbers || pwSpecial) {
+      if (pwLowerCase == true|| pwUpperCase == true || pwNumbers == true || pwSpecial == true) {
         i = 1;
       } else {
         alert("One Character type should be selected!");
       }
-    // while (i === 1) {
+    }
 
-    // }
+    // One condition chosen.
+    if (pwLowerCase == true && pwUpperCase == false && pwNumbers == false && pwSpecial == false) {
+      var chars = 'abcdefghijklmnopqrstuvwxy';
+      lcPass = '';
+      for (var n = 0; n <= pwLength; ++n) {
+        var c = Math.floor(Math.random()*chars.length + 1);
+        lcPass += chars.charAt(c);
+      }
+      return lcPass;
+    }
+    if (pwLowerCase == false && pwUpperCase == true && pwNumbers == false && pwSpecial == false) {
+      var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      lcPass = '';
+      for (var n = 0; n <= pwLength; ++n) {
+        var c = Math.floor(Math.random()*chars.length + 1);
+        lcPass += chars.charAt(c);
+      }
+      return lcPass;
+    }
+    if (pwLowerCase == false && pwUpperCase == false && pwNumbers == true && pwSpecial == false) {
+      var chars = '0123456789';
+      lcPass = '';
+      for (var n = 0; n <= pwLength; ++n) {
+        var c = Math.floor(Math.random()*chars.length + 1);
+        lcPass += chars.charAt(c);
+      }
+      return lcPass;
+    }
+    if (pwLowerCase == false && pwUpperCase == false && pwNumbers == false && pwSpecial == true) {
+      var chars = '~`!@#$%^&*()_-+={[}]|:;<,>.?/';
+      lcPass = '';
+      for (var n = 0; n <= pwLength; ++n) {
+        var c = Math.floor(Math.random()*chars.length + 1);
+        lcPass += chars.charAt(c);
+      }
+      return lcPass;
+    }
+
+    // Two conditions chosen.
+    if (pwLowerCase == true && pwUpperCase == true && pwNumbers == false && pwSpecial == false) {
+      var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxy';
+      lcPass = '';
+      for (var n = 0; n <= pwLength; ++n) {
+        var c = Math.floor(Math.random()*chars.length + 1);
+        lcPass += chars.charAt(c);
+      }
+      return lcPass;
+    }
+    if (pwLowerCase == true && pwUpperCase == false && pwNumbers == true && pwSpecial == false) {
+      var chars = 'abcdefghijklmnopqrstuvwxy0123456789';
+      lcPass = '';
+      for (var n = 0; n <= pwLength; ++n) {
+        var c = Math.floor(Math.random()*chars.length + 1);
+        lcPass += chars.charAt(c);
+      }
+      return lcPass;
+    }
+    if (pwLowerCase == true && pwUpperCase == false && pwNumbers == false && pwSpecial == true) {
+      var chars = 'abcdefghijklmnopqrstuvwxy~`!@#$%^&*()_-+={[}]|:;<,>.?/';
+      lcPass = '';
+      for (var n = 0; n <= pwLength; ++n) {
+        var c = Math.floor(Math.random()*chars.length + 1);
+        lcPass += chars.charAt(c);
+      }
+      return lcPass;
+    }
+    if (pwLowerCase == false && pwUpperCase == true && pwNumbers == true && pwSpecial == false) {
+      var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXY0123456789';
+      lcPass = '';
+      for (var n = 0; n <= pwLength; ++n) {
+        var c = Math.floor(Math.random()*chars.length + 1);
+        lcPass += chars.charAt(c);
+      }
+      return lcPass;
+    }
+    if (pwLowerCase == false && pwUpperCase == true && pwNumbers == false && pwSpecial == true) {
+      var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXY~`!@#$%^&*()_-+={[}]|:;<,>.?/';
+      lcPass = '';
+      for (var n = 0; n <= pwLength; ++n) {
+        var c = Math.floor(Math.random()*chars.length + 1);
+        lcPass += chars.charAt(c);
+      }
+      return lcPass;
+    }
+    if (pwLowerCase == false && pwUpperCase == false && pwNumbers == true && pwSpecial == true) {
+      var chars = '0123456789~`!@#$%^&*()_-+={[}]|:;<,>.?/';
+      lcPass = '';
+      for (var n = 0; n <= pwLength; ++n) {
+        var c = Math.floor(Math.random()*chars.length + 1);
+        lcPass += chars.charAt(c);
+      }
+      return lcPass;
+    }
+
+    // 3 conditions chosen
+    if (pwLowerCase == true && pwUpperCase == true && pwNumbers == true && pwSpecial == false) {
+      var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxy0123456789';
+      lcPass = '';
+      for (var n = 0; n <= pwLength; ++n) {
+        var c = Math.floor(Math.random()*chars.length + 1);
+        lcPass += chars.charAt(c);
+      }
+      return lcPass;
+    }
+    if (pwLowerCase == true && pwUpperCase == true && pwNumbers == false && pwSpecial == true) {
+      var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxy~`!@#$%^&*()_-+={[}]|:;<,>.?/';
+      lcPass = '';
+      for (var n = 0; n <= pwLength; ++n) {
+        var c = Math.floor(Math.random()*chars.length + 1);
+        lcPass += chars.charAt(c);
+      }
+      return lcPass;
+    }
+    if (pwLowerCase == true && pwUpperCase == false && pwNumbers == true && pwSpecial == true) {
+      var chars = 'abcdefghijklmnopqrstuvwxy0123456789~`!@#$%^&*()_-+={[}]|:;<,>.?/';
+      lcPass = '';
+      for (var n = 0; n <= pwLength; ++n) {
+        var c = Math.floor(Math.random()*chars.length + 1);
+        lcPass += chars.charAt(c);
+      }
+      return lcPass;
+    }
+    if (pwLowerCase == false && pwUpperCase == true && pwNumbers == true && pwSpecial == true) {
+      var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~`!@#$%^&*()_-+={[}]|:;<,>.?/';
+      lcPass = '';
+      for (var n = 0; n <= pwLength; ++n) {
+        var c = Math.floor(Math.random()*chars.length + 1);
+        lcPass += chars.charAt(c);
+      }
+      return lcPass;
+    }
+
+    // All conditions chosen.
+    if (pwLowerCase == true && pwUpperCase == true && pwNumbers == true && pwSpecial == true) {
+      var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxy0123456789~`!@#$%^&*()_-+={[}]|:;<,>.?/';
+      lcPass = '';
+      for (var n = 0; n <= pwLength; ++n) {
+        var c = Math.floor(Math.random()*chars.length + 1);
+        lcPass += chars.charAt(c);
+      }
+      return lcPass;
     }
   }
 }  
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
